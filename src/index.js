da0e66e4e7709851b5334b879e353eb913dd47cf
Camila Fuentes Maldonado <camilafuentesmaldonado@gmail.com>
2022-10-25 11:47:37 -0300

    agregamos la estructura basica de index para backend

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
-
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
 //para usar express que es el bootstrap del lado del bakcend
 //creo una instancia de express
 //tiwene que ser express con miniuscila
@@ -11,4 +13,22 @@ app.listen(app.get('port'), ()=>{
     console.log('estoy en el puerto ' + app.get('port'))
 })
 
-console.log('hola mundo');
+console.log('hola mundo cami');
+
+//middlewares: funciones que se ejecutan antes de las rutas
+// morgan da informascion extra enl a terminal
+ app.use(morgan('dev'));
+ //tenemos que permitir peticiones remotas
+ app.use(cors())
+ //agregart middlewares para poder ejecutar json
+ app.use(express.json())
+ app.use(express.urlencoded({extended:true}))
+//cargar un archivo estático
+app.use(express.static(path.join(__dirname, '../public')))
+console.log()
+
+//rutas:nombre de dominio + ---
+
+app.get('/prueba', (req, res)=>{
+ res.send('esto es una prueba de la peticion get')
+})
