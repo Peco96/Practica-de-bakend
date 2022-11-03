08a43050b36aad6c2acecd366b9c569a6acceaf4
´Peco´ <pecohernandez6@gmail.com>
2022-11-03 16:53:00 -0300

    agregados

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
-const url = "mongodb://localhost:27017/cafe-branch";
-
+//const url = "mongodb://localhost:27017/cafe-branch";
+const url = "mongodb+srv://Peco:peco43822@cluster0.pvqfnt9.mongodb.net/cafe-branch-c6i";
 mongoose.connect(url);
 
 const connection = mongoose.connection;
